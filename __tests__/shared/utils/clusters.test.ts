dfb1e24e2250b8067da3bee8bab564b154646664
Miguel Hernandez <miguelh72@outlook.com>
2021-10-18 18:02:54 -0400

    added test to verify clustering algorithm for step and linear pdf
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- __tests__/shared/utils/clusters.test.ts
+++ __tests__/shared/utils/clusters.test.ts
@@ -27,9 +27,9 @@ describe('Correctness of clustering algorithm using step function pdf', () => {
 
   test('Should return that endpoints with same pdf belongs in the same cluster', async () => {
     const endpointsPDF: EndpointPDF[] = [
-      { method: 'GET', endpoint: '/api', pdf: () => 1 / 24 },
-      { method: 'GET', endpoint: '/api/2/', pdf: (x) => x / 24 },
-      { method: 'GET', endpoint: '/api/3/', pdf: (x) => x / 24 },
+      { method: 'GET', endpoint: '/api/1', pdf: () => 1 / 24 },
+      { method: 'GET', endpoint: '/api/2', pdf: (x) => x / 24 },
+      { method: 'GET', endpoint: '/api/3', pdf: (x) => x / 24 },
       { method: 'POST', endpoint: '/api/4', pdf: () => 1 / 24 }
     ];
     const callDist: DistributionFunction = () => 100;
@@ -39,17 +39,95 @@ describe('Correctness of clustering algorithm using step function pdf', () => {
 
     expect(clusters).toHaveLength(2);
     // no guarantee on return clustering order
-    if (clusters[0][1].endpoint === '/api' || clusters[0][1].endpoint === '/api/4') {
-      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api' });
+    if (clusters[0][1].endpoint === '/api/1' || clusters[0][1].endpoint === '/api/4') {
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/1' });
       expect(clusters[0]).toContainEqual({ method: 'POST', endpoint: '/api/4' });
-      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/2/' });
-      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/3/' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/2' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/3' });
     } else {
-      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/1' });
       expect(clusters[1]).toContainEqual({ method: 'POST', endpoint: '/api/4' });
-      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/2/' });
-      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/3/' });
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/2' });
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/3' });
     }
+  });
+
+  test('Should return that endpoints with similar pdf belongs in the same cluster', async () => {
+    const endpointsPDF: EndpointPDF[] = [
+      { method: 'GET', endpoint: '/api/1', pdf: () => 0.7 / 24 },
+      { method: 'GET', endpoint: '/api/2', pdf: (x) => 0.7 * x / 24 },
+      { method: 'GET', endpoint: '/api/3', pdf: (x) => x / 24 },
+      { method: 'POST', endpoint: '/api/4', pdf: () => 1 / 24 }
+    ];
+    const callDist: DistributionFunction = () => 100;
+    const singleEndpointServerResponses: Endpoint[] = simulateServerResponses(endpointsPDF, callDist, 5, 60);
+
+    const clusters: Cluster[] = determineClusters(singleEndpointServerResponses);
+
+    expect(clusters).toHaveLength(2);
+    // no guarantee on return clustering order
+    if (clusters[0][1].endpoint === '/api/1' || clusters[0][1].endpoint === '/api/4') {
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/1' });
+      expect(clusters[0]).toContainEqual({ method: 'POST', endpoint: '/api/4' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/2' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/3' });
+    } else {
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/1' });
+      expect(clusters[1]).toContainEqual({ method: 'POST', endpoint: '/api/4' });
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/2' });
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/3' });
+    }
+  });
+
+  test('Should return that endpoints with same total calls but different timing pdf belongs in the same cluster', async () => {
+    function leftStep(x) {
+      if (x < 12) return 1 / 12;
+      return 0;
+    }
+    function rightStep(x) {
+      if (x > 12) return 1 / 12;
+      return 0;
+    }
+
+    const endpointsPDF: EndpointPDF[] = [
+      { method: 'GET', endpoint: '/api/1', pdf: leftStep },
+      { method: 'GET', endpoint: '/api/2', pdf: leftStep },
+      { method: 'GET', endpoint: '/api/3', pdf: rightStep },
+      { method: 'POST', endpoint: '/api/4', pdf: rightStep }
+    ];
+    const callDist: DistributionFunction = () => 100;
+    const singleEndpointServerResponses: Endpoint[] = simulateServerResponses(endpointsPDF, callDist, 5, 60);
+
+    const clusters: Cluster[] = determineClusters(singleEndpointServerResponses);
+
+    expect(clusters).toHaveLength(2);
+    // no guarantee on return clustering order
+    if (clusters[0][1].endpoint === '/api/1' || clusters[0][1].endpoint === '/api/2') {
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/1' });
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/2' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/3' });
+      expect(clusters[1]).toContainEqual({ method: 'POST', endpoint: '/api/4' });
+    } else {
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/1' });
+      expect(clusters[1]).toContainEqual({ method: 'GET', endpoint: '/api/2' });
+      expect(clusters[0]).toContainEqual({ method: 'GET', endpoint: '/api/3' });
+      expect(clusters[0]).toContainEqual({ method: 'POST', endpoint: '/api/4' });
+    }
+  });
+
+  test('Should return that endpoints with similar pdf belongs in the same cluster, even if just one cluster. No need for microservice result.', async () => {
+    const endpointsPDF: EndpointPDF[] = [
+      { method: 'GET', endpoint: '/api/1', pdf: () => 1 },
+      { method: 'GET', endpoint: '/api/2', pdf: () => 0.9 },
+      { method: 'GET', endpoint: '/api/3', pdf: () => 1.2 },
+      { method: 'POST', endpoint: '/api/4', pdf: () => 0.85 }
+    ];
+    const callDist: DistributionFunction = () => 100;
+    const singleEndpointServerResponses: Endpoint[] = simulateServerResponses(endpointsPDF, callDist, 5, 60);
+
+    const clusters: Cluster[] = determineClusters(singleEndpointServerResponses);
+
+    expect(clusters).toHaveLength(1);
   });
 
   // TODO thoroughly test clustering results to different types of biasing pdf functions
