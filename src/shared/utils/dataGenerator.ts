73903793afa457617201b2fc98022324db2ceff4
Miguel Hernandez <miguelh72@outlook.com>
2021-10-17 21:44:25 -0400

    Implemented data generator utility. It is passing tests for uniform distributions
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- src/shared/utils/dataGenerator.ts
+++ src/shared/utils/dataGenerator.ts
@@ -1,7 +1,3 @@
-/**
- * Utility used to generate endpoint biased data sets.
- */
-
 import { Endpoint } from './../models/endpointModel';
 
 export type DistributionFunction = (x: number) => number;
@@ -13,14 +9,18 @@ export type EndpointPDF = {
 }
 
 /**
- * TODO
+ * Simulate the server responding to client calls at the specified endpoints. Algorithm is probabilistic. You specify the probability distribution function (pdf) as a function
+ * for each endpoint in a 24 hour period as well as the overall server call distribution in a 24 hour period and calls are made randomly taking into account the pdf of each
+ * endpoint.
  * 
- * @param endpoints 
- * @param numCallsDist 
- * @param numDays 
- * @param granularity 
+ * @param {EndpointPDF[]} endpoints - Array of EndpointPDF which include method, endpoint, and pdf.
+ * @param {DistributionFunction} numCallsDist - Distribution function of number of calls server received over a 24 hour period.
+ * @param {number} numDays - Number of days to run simulation for.
+ * @param {number} granularity - Granularity, in minutes, of internal calculation period. Defaults to 30 minutes. Smaller values means approximation is closer to continuos pdf 
+ * since algorithm uses trapezoid rule numerical integration. Warning of using too small granularity: if number of calls for a granularity interval sums to less than one, no 
+ * calls will be made for period.
  * 
- * @returns
+ * @returns Array of Endpoints that simulate how a real life server would have responded given the endpoint bias in pdf.
  * 
  * @public
  */
@@ -40,6 +40,20 @@ export function simulateServerResponses(endpoints: EndpointPDF[], numCallsDist: 
   return simulatedCalls;
 }
 
+/**
+ * Simulate the server responding to client calls for a period of 24 hours.
+ * 
+ * @param {EndpointPDF} endpoints - Array of EndpointPDF which include method, endpoint, and pdf.
+ * @param {DistributionFunction} numCallsDist - Distribution function of number of calls server received over a 24 hour period.
+ * @param {number} granularity - Granularity, in minutes, of internal calculation period. Defaults to 30 minutes. Smaller values means approximation is closer to continuos pdf 
+ * since algorithm uses trapezoid rule numerical integration. Warning of using too small granularity: if number of calls for a granularity interval sums to less than one, no 
+ * calls will be made for period.
+ * @param {Date} dayStart - Date indicating start of 24 hour period to be used as a full day.
+ * 
+ * @returns Array of Endpoints that simulate how a real life server would have responded given the endpoint bias in pdf.
+ * 
+ * @private
+ */
 function simulateSingleDayResponses(endpoints: EndpointPDF[], numCallsDist: DistributionFunction, granularity: number, dayStart: Date): Endpoint[] {
   const numIntervals = (24 * 60) / granularity;
   const intervalStartTime = new Date(dayStart);
@@ -94,6 +108,8 @@ function simulateSingleDayResponses(endpoints: EndpointPDF[], numCallsDist: Dist
  * @param {DistributionFunction} fn - Function to be integrated between intervalStartTime and intervalEndTime.
  * @param {number} intervalStartTime - Position on the x-axis at which to begin integration.
  * @param {number} intervalEndTime - Position on the x-aix at which to end integration.
+ * 
+ * @returns {number} - area under curve between start and stop.
  * 
  * @private
  */
