27975dc3025a13f73d7ef737db36f2336546f745
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-10-19 19:00:08 -0700

    MUI theme, light and dark mode.

---
+++ src/client/components/Theme.tsx
@@ -0,0 +1,43 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#3559a4',
+    },
+    secondary: {
+      main: '#28a78d',
+    },
+    error: {
+      main: '#e24b44',
+    },
+    warning: {
+      main: '#f7b646',
+    },
+    divider: '#6C6C6C',
+  },
+};
+
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#3559a4',
+    },
+    secondary: {
+      main: '#28a78d',
+    },
+    error: {
+      main: '#e24b44',
+    },
+    warning: {
+      main: '#f7b646',
+    },
+    divider: '#6C6C6C',
+    info: {
+      main: '#F7B646',
+    },
+  },
+};
