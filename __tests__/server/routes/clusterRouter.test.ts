eeb73c1187207efaf98045f5f6377cb31b7fbbcd
Parker Hutcheson <pdhutcheson@gmail.com>
2021-10-22 18:36:20 -0700

    Added MongoDb testing database

--- __tests__/server/routes/clusterRouter.test.ts
+++ __tests__/server/routes/clusterRouter.test.ts
@@ -2,15 +2,17 @@ import { connect, disconnect } from '../../../src/shared/models/mongoSetup';
 import { EndpointModel, Endpoint} from './../../../src/shared/models/endpointModel';
 import request from 'supertest';
 import app from './../../../src/server/index';
+import { MONGODB_URI_TESTING } from './../../../src/server/secrets.json';
+
 
 describe('test out all our routes', () => {
   jest.setTimeout(1 * 60 * 1000); //Sets a 60 second
 
 beforeAll(async () => {
-  await connect('mongodb+srv://admin:test@cluster0.dopf4.mongodb.net/DePaul?retryWrites=true&w=majority');
-  app.get('/', (req, res) => res.status(200).send([{ method: 'GET', endpoint: '/api/example' }, { method: 'POST', endpoint: '/api/example2' }]));
-  app.get('/load/1', (req, res) => res.status(200).send({ x: [1, 20, 12], y: [420, 300, 200]}));
-  app.get('/tree/1', (req, res) => res.status(200).send({name: 'Cluster1', children: [{name: 'get', children: [{name: '/api/login'}, {name:'/api/logout'}]}]}));
+  await connect(MONGODB_URI_TESTING);
+  // app.get('/', (req, res) => res.status(200).send([{ method: 'GET', endpoint: '/api/example' }, { method: 'POST', endpoint: '/api/example2' }]));
+  // app.get('/load/1', (req, res) => res.status(200).send({ x: [1, 20, 12], y: [420, 300, 200]}));
+  // app.get('/tree/1', (req, res) => res.status(200).send({name: 'Cluster1', children: [{name: 'get', children: [{name: '/api/login'}, {name:'/api/logout'}]}]}));
   await EndpointModel.deleteMany();
 });
 
