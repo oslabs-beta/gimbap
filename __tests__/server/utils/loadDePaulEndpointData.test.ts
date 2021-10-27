675c3728266aeed9a00662b44852b42c59a056d4
Miguel Hernandez <miguelh72@outlook.com>
2021-10-27 18:57:18 -0400

    added pre-processing while DePaul data is loaded to database
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- __tests__/server/utils/loadDePaulEndpointData.test.ts
+++ __tests__/server/utils/loadDePaulEndpointData.test.ts
@@ -3,14 +3,18 @@ import path from 'path';
 
 import loadDePaulEndpointData from './../../../src/server/utils/loadDePaulEndpointData';
 import { EndpointModel, Endpoint } from './../../../src/shared/models/endpointModel';
+import { startWatchingEndpointModel } from './../../../src/server/models/endpointBucketsModel';
 import { connect, disconnect } from '../../../src/shared/models/mongoSetup';
+
+import { MONGODB_URI } from './../../../src/server/secrets.json';
 
 xdescribe('Populate database with DePaul CTI data and verify data is in DB', () => {
   jest.setTimeout(15 * 60 * 1000);
 
   beforeAll(async () => {
-    await connect('mongodb+srv://admin:test@cluster0.dopf4.mongodb.net/DePaul?retryWrites=true&w=majority');
+    await connect(MONGODB_URI);
     await EndpointModel.deleteMany({});
+    await startWatchingEndpointModel();
   });
 
   afterAll(async () => {
@@ -19,12 +23,14 @@ xdescribe('Populate database with DePaul CTI data and verify data is in DB', () 
   });
 
   test('Check for a specific data point', async () => {
-    await loadDePaulEndpointData();
+    const batchSize = 1000;
+
+    await loadDePaulEndpointData(batchSize);
 
     const data = await fs.readFile(path.resolve('cti-april02-log.txt'), 'utf8');
     const entries: string[] = data.split('\n').slice(4);
 
-    for (let i = 0; i < entries.length; i += 1000) {
+    for (let i = 0; i < entries.length; i += batchSize) {
       const entry: string[] = data.split('\n').slice(4);
       const timeStamp: number = new Date(entry[0] + ' ' + entry[1]).getTime();
       const expected: Endpoint = {
