eb390b4b8aa961ac0b10ffab3cfade5f1e18b35d
Khandker <khandker.islam46@gmail.com>
2021-10-28 11:36:52 -0400

    Got rid of git single row

--- src/client/components/clusters/treegraph/EndpointList.tsx
+++ src/client/components/clusters/treegraph/EndpointList.tsx
@@ -31,7 +31,7 @@ export default function EndpointList(props) {
     return (
       <ListItem style={style} key={index} component="div" disablePadding>
         <ListItemButton>
-          <ListItemText primary={`${index} : ${endpointList[index]?.name}`} />
+          <ListItemText primary= {endpointList.length === 0 ? 'No endpoints to show...' : endpointList[index]?.name} />
         </ListItemButton>
       </ListItem>
     );
@@ -60,7 +60,7 @@ export default function EndpointList(props) {
           itemCount={endpointList.length ? endpointList.length: 1}
           overscanCount={5}
         >
-          {endpointList.length === 0 ? renderSingleRow : renderRow}
+          {renderRow}
         </FixedSizeList>
       </Box>
     );
