a3147ef87dd2e58002dfc1ec1be5140e13e2aa54
Khandker <khandker.islam46@gmail.com>
2021-11-09 20:16:59 -0500

    Initial electron build setup

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const path = require( 'path' );
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -6,7 +7,7 @@ function createWindow () {
     height: 600
   });
 
-  win.loadFile('../client/index.html');
+  win.loadFile( path.resolve( __dirname, 'client/index.html' ) );
 }
 
 // createWindow();
