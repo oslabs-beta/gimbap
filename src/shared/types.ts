d8832f3ca212586448571a869e2aba2742cbcecc
Miguel Hernandez <miguelh72@outlook.com>
2021-11-10 13:36:19 -0500

    updated comments
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,14 @@
+/**
+ * A cluster is an array of Route objects. It represents a recommended clustering of server routes.
+ */
 export type Cluster = Route[];
 
+/**
+ * Defined a server endpoint as the combination of the HTTP method and the relative URL.
+ * 
+ * @property method - HTTP method type
+ * @property endpoint - HTTP request relative endpoint
+ */
 export type Route = {
   method: string;
   endpoint: string;
